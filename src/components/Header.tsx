4663f17716bdd818038d3a98c37b48d143bd422a
Brice Suazo <bricesuazo@gmail.com>
2023-01-03 00:37:55 +0800

    Fix header. Remove logo-dark.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTheme } from "next-themes";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { useState, useEffect } from "react";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  // When mounted on client, now we can show the UI
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null;
 
   return (
     <header className="fixed top-0 flex w-full flex-col items-center p-2">
@@ -20,19 +13,11 @@ const Header = () => {
         className="mx-auto flex flex-col items-center rounded-full p-2"
       >
         <Image
-          src={`/images/logo-light.png`}
+          src="/images/logo-light.png"
           alt="Brice Suazo's Logo"
           width={48}
           height={48}
-          className="hidden dark:block"
-          priority
-        />
-        <Image
-          src={`/images/logo-dark.png`}
-          alt="Brice Suazo's Logo"
-          width={48}
-          height={48}
-          className="block dark:hidden"
+          className="invert dark:invert-0"
           priority
         />
         <h5 className="font-bold">Brice Suazo</h5>
