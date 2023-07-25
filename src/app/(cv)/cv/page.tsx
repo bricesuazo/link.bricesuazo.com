55b13047ad5699df7ef362c714adf5d0ef5170c7
Brice Suazo <bricesuazo@gmail.com>
2023-07-25 12:33:20 +0800

    Add frame to /cv

--- src/app/(cv)/cv/page.tsx
+++ src/app/(cv)/cv/page.tsx
@@ -2,13 +2,12 @@
 
 export default function CV() {
   return (
-    <>
-      {/* <iframe src="Brice Suazo - CV 2023.pdf" width="100%" height="1000px" /> */}
-      {/* <iframe
-        src="https://drive.google.com/viewerng/viewer?embedded=true&url=./Brice Suazo - CV 2023.pdf#toolbar=0&scrollbar=0"
+    <div className="h-screen">
+      <iframe
+        src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://raw.githubusercontent.com/bricesuazo/link.bricesuazo.com/main/public/Brice%20Suazo%20-%20CV%202023.pdf#toolbar=0&scrollbar=0"
         height="100%"
         width="100%"
-      ></iframe> */}
-    </>
+      />
+    </div>
   );
 }
